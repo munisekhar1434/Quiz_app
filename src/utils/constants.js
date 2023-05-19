86d030a00b127de151cff4c2a665528dd034b405
munisekhar1434 <kuntumallamunisekhar2000@gmail.com>
2023-05-19 17:38:13 +0530

    changes4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
 export const quiz = [
   {
     question:
-      "what was the sum 1+2+3+4+5",
+      "what was the sum 1+2+3+4+5 ?",
     choices: ["12", "15", "17", "13"],
     correctAnswer: "15",
   },
   {
     question:
-      "which telugu movie song got 2023 oscar ",
+      "which telugu movie song got 2023 oscar? ",
     choices: ["RRR", "RR", "BBB", "RRRR"],
     correctAnswer: "RRR",
   },
