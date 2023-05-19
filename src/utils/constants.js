80617d02cc164f86e8d5ec13466506f56d010a95
munisekhar1434 <kuntumallamunisekhar2000@gmail.com>
2023-05-19 17:37:10 +0530

    changes4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,24 +1,24 @@
 export const quiz = [
   {
     question:
-      "Variables declared with the let keyword have what type of scope?",
-    choices: ["function scope", "block scope", "inline scope", "global scope"],
-    correctAnswer: "block scope",
+      "what was the sum 1+2+3+4+5",
+    choices: ["12", "15", "17", "13"],
+    correctAnswer: "15",
   },
   {
     question:
-      "Which attribute must have a unique value each time it is used in an HTML document?",
-    choices: ["title", "class", "style", "id"],
-    correctAnswer: "id",
+      "which telugu movie song got 2023 oscar ",
+    choices: ["RRR", "RR", "BBB", "RRRR"],
+    correctAnswer: "RRR",
   },
   {
-    question: "What does the === comparison operator do?",
+    question: "which was different in this?",
     choices: [
-      "It sets one variable equal to another in both value and type",
-      "It tests for equality of type only",
-      "It tests for equality of value only",
-      "It tests for equality of value and type",
+      "int",
+      "char",
+      "string",
+      "Hello",
     ],
-    correctAnswer: "It tests for equality of value and type",
+    correctAnswer: "Hello",
   },
 ];
